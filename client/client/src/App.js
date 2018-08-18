c49cfd8679c9bb18a1b739545ce45657a19cf646
ena.sun <ena.sun@xero.com>
2018-08-19 11:06:02 +1200

    aDDED A MAP

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactMapboxGl, { Layer, Feature } from 'react-mapbox-gl';
 
 class App extends Component {
   state = {mesh: {}}
+  
 
   componentDidMount() {
     fetch('/mesh?lat=4&long=2')
@@ -11,6 +13,8 @@ class App extends Component {
       .then(mesh => this.setState({ mesh }));
   }
   render() {
+    const Map = ReactMapboxGl({ accessToken:'pk.eyJ1Ijoic2F2aW5ncHZ0cnlhbiIsImEiOiJjamt6YWpxaXgwczZ2M3BvNnh3dDIzb3VlIn0.qheYYbnMlTV-w04LkzKfMw' });
+    const zoom = [8];
     return (
       <div className="App">
         <header className="App-header">
@@ -19,9 +23,23 @@ class App extends Component {
         <p className="App-intro">
           {this.state.mesh.numberOfFamilies}
         </p>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+            height: "100vh",
+            width: "100vw"
+          }}>
+            <Layer
+              type="symbol"
+              id="marker"
+              layout={{ "icon-image": "marker-15" }}>
+              <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+            </Layer>
+        </Map>
       </div>
     );
   }
 }
+
 
 export default App;
