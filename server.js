bc0a1e878042df9403172fb564a0fca1c3fa58bc
Alexandra Fowler <alexandrafowler24@gmail.com>
2018-08-19 13:52:08 +1200

    Completed search in json and added full json data file

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-var data = fs.readFileSync('data/dummy.json', 'utf8')
+var data = fs.readFileSync('data/2013FamilyData.json', 'utf8')
 var census = JSON.parse(data)
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'client/client/build')))
 
-var meshblock = 'MB 0000200';
 
 app.get('/', (req, res) => res.send("Welcome to our dummy home page :)"))
 app.get('/mesh', (req, res) => {
 	console.log(req.query)
-res.json(census)
+	if (req.query.lat == 4){
+		const meshblock = "MB 1021000"
+		console.log(returnObj(meshblock))
+		res.json(meshblock)
+	}
 })
 
 app.listen(process.env.PORT || 3000)
 
-console.log(census);
+
+function returnObj(meshblock) {
+	const ret = census.filter(x => x.meshblockCode == meshblock)
+	return ret;
+}
+
+
+// function searchMeshblock(meshblockCode) {
+
+// }
+
 // const csvFilePath = 'C:/Users/Ryan/Documents/GitHub/RAD-CreateCamp/server/csv-file/test.csv';
 // const csv = require('csvtojson')
 //
